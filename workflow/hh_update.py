61717244fea6f1b0af4596c0c56c89e2015f3c3d
madalbekovich <talasbekalgaziev@gmail.com>
2024-11-23 04:54:05 +0600

    addchanges

--- workflow/hh_update.py
+++ workflow/hh_update.py
@@ -1,5 +1,7 @@
 import requests
 from colorama import Fore, Style
+import os
+import math
 import time 
 ascii_art = f"""       
 
@@ -38,10 +40,22 @@ ascii_art = f"""
                                                                                                           
 """
 
-
 for char in ascii_art:
     print(char, end='', flush=True)
     time.sleep(0.0010)
+    
+def load_responded_vacancies(file_path='vacancies_responded.txt'):
+    """Загружает список откликанных вакансий из файллф"""
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            data = file.read()
+            return set(data.strip().split(',')) if data else set()
+    return set()
+
+def save_responded_vacancy(vacancy_id, file_path='vacancies_responded.txt'):
+    """Добавляет вакансию в список откликанных и сохраняет в файл."""
+    with open(file_path, 'a') as file:
+        file.write(f'{vacancy_id} - ')
 
 
 def get_vacancies(AUTH_KEY, FILTER_URL):
@@ -65,20 +79,28 @@ def click_vacancies(AUTH_KEY, RESUME_ID, MESSAGE=None, FILTER_URL=None):
     POST_URL = 'https://api.hh.ru/negotiations?hhtmSource=vacancy_response&hhtmFrom=main&host=headhunter.kg&locale=RU'
     headers = {'Authorization': f'Bearer {AUTH_KEY}'}
     
-    vacancies = get_vacancies(AUTH_KEY=AUTH_KEY, FILTER_URL=FILTER_URL)
+    vacancies = get_vacancies(AUTH_KEY=AUTH_KEY, FILTER_URL=DEFAULT_URL)
+    responded_vacancies = load_responded_vacancies()
     
     if vacancies:
         for vacancy_id in vacancies:
+            if vacancy_id in responded_vacancies:
+                print(Fore.RED + f'Пропускаем вакансию {vacancy_id},которую вы уже откликались.')
+                continue
             data = {'vacancy_id': vacancy_id, 'resume_id': RESUME_ID, 'message': MESSAGE, 'with_chat_info': True}
-            print(f'Отправляем запрос: {data}')
+            # print(f'Отправляем запрос: {data}')
             
             response = requests.post(POST_URL, data=data, headers=headers)
             try:
                 if response.status_code == 201:
                     print(f'Отклик на вакансию: {vacancy_id}')
+                    save_responded_vacancy(vacancy_id)
                 else:
+                    responses_status = response.json()
+                    if (responses_status.get('description')) == 'Daily negotiations limit is exceeded':
+                        print(Fore.RED + f'Превышен суточный лимит откликов,\n{Fore.YELLOW + Style.BRIGHT}Попробуйте указать другое резюме ㋡')
+                        break
                     print(f'Не удалось откликнуться. Ошибка с кодом: {response.status_code}')
-                    print(response.text)
             except Exception as e:
                 print(f'ERROR: {e}')
     else:
@@ -87,6 +109,13 @@ def click_vacancies(AUTH_KEY, RESUME_ID, MESSAGE=None, FILTER_URL=None):
 RESUME_ID = input(Fore.GREEN + Style.BRIGHT + 'ID резюме: ')
 MESSAGE = input(Fore.GREEN + Style.BRIGHT + 'Желаемая сообщение работадателям: ')
 AUTH_KEY = input(Fore.GREEN + Style.BRIGHT + 'Введите токен авторизации: ')
-FILTER_URL = input(Fore.GREEN + Style.BRIGHT + 'Введите URL фильтрации: ')
+KEY_STACK = input(Fore.GREEN + Style.BRIGHT + 'Введите ключёвые слова(например: Python разработчик): ')
 
-click_vacancies(AUTH_KEY=AUTH_KEY, RESUME_ID=RESUME_ID, MESSAGE=MESSAGE, FILTER_URL=FILTER_URL)
+#TO:DO
+# PAGE_COUNT = int(input(Fore.GREEN + Style.BRIGHT + 'Введите кол-во откликов: '))
+# round_page = PAGE_COUNT / 100
+# per_page = math.ceil(round_page)
+# for page in range(per_page):
+
+DEFAULT_URL = f'https://api.hh.ru/vacancies?host=headhunter.kg&page=2&per_page=100&text={KEY_STACK}'
+click_vacancies(AUTH_KEY=AUTH_KEY, RESUME_ID=RESUME_ID, MESSAGE=MESSAGE, FILTER_URL=DEFAULT_URL)
